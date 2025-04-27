76b7cd73de64c9acc5a00197ca9eeae957005d10
JaimeAyoub <jaime.ayoub@saei.mx>
2025-04-27 17:58:46 -0600

    Animation and FlashLight
    Se descargo el modelo de un teletubi y con sus animaciones.
    Ademas se hizo un prototipo de linterna

---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class FlashLight : MonoBehaviour
+{
+    public GameObject Light;
+    public CinemachineVirtualCameraBase camera;
+    public Light LightFont;
+    public bool isOn = false;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveLight();
+        PowerLight();
+    }
+
+    // ReSharper disable Unity.PerformanceAnalysis
+    void MoveLight()
+    {
+        float X = camera.GetComponent<CinemachinePanTilt>().PanAxis.Value;
+        float Y = camera.GetComponent<CinemachinePanTilt>().TiltAxis.Value;
+        
+        Light.transform.rotation = Quaternion.Euler(Y,X,0);
+    }
+
+    void PowerLight()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isOn = !isOn;
+            LightFont.enabled = isOn;
+        }
+    }
+}
